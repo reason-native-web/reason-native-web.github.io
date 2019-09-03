ddf64d59ba9c782bfe2c118587aa686bc8d360fd
Ulrik <ulrik.strid@outlook.com>
2019-09-03 20:26:24 +0200

    Set config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,17 +12,17 @@
 const users = [];
 
 const siteConfig = {
-  title: "Test Site", // Title for your website.
-  tagline: "A website for testing",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
+  title: "Reason Native Web", // Title for your website.
+  tagline: "A toolkit for native web development in OCaml and Reason",
+  url: "https://reason-native-web.github.io", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "test-site",
-  organizationName: "facebook",
+  projectName: "reason-native-web.github.io",
+  organizationName: "reason-native-web",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
